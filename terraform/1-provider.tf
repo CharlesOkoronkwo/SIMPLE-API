0348a33785e1ecffdb47d6fcae438c0bbf3f823f
Charles <135387185+CharlesOkoronkwo@users.noreply.github.com>
2024-09-06 06:29:34 -0500

    Update 1-provider.tf

--- terraform/1-provider.tf
+++ terraform/1-provider.tf
@@ -17,5 +17,5 @@
 provider "google" {
   project = "simple-api-434410"
   region  = "us-central1"
-  impersonate_service_account = "github-wif@simple-api-434410.iam.gserviceaccount.com"
+  impersonate_service_account = "simple-api-svc-acct@simple-api-434410.iam.gserviceaccount.com"
 }
