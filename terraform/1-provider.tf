fa7028cc5febddd55512303f6fd6f91e1c7af278
Charles <135387185+CharlesOkoronkwo@users.noreply.github.com>
2024-09-06 07:59:09 -0500

    Update 1-provider.tf

--- terraform/1-provider.tf
+++ terraform/1-provider.tf
@@ -17,11 +17,5 @@
 provider "google" {
   project     = "simple-api-434410"
   region      = "us-central1"
-  credentials = base64decode(var.google_credentials)
-}
-
-variable "google_credentials" {
-  description = "Base64 encoded service account key"
-  type        = string
 }
 
