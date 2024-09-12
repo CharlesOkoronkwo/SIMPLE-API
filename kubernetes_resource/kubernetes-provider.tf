6ed645ddc27f5d7234cc806255eb3c363b4410c4
Charles <135387185+CharlesOkoronkwo@users.noreply.github.com>
2024-09-11 19:55:53 -0500

    Updated kubernetes-provider.tf

--- kubernetes_resource/kubernetes-provider.tf
+++ kubernetes_resource/kubernetes-provider.tf
@@ -8,6 +8,16 @@ terraform {
 }
 
 provider "kubernetes" {
-  # Configuration options
-  host  = "https://${data.google_container_cluster.cluster.endpoint}"
+  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
 }
+
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "simple-api"
+  location = "us-central1"
+  project  = "simple-api-434410"
+}
+
