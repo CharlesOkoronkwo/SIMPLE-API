ab6038eab0b57239976e6d87fc0a61c53eeca979
Taylor <admin@admins-MacBook-Pro.local>
2025-01-30 03:03:58 -0500

    updated ci-cd workflow and gke output

--- modules/gke/output.tf
+++ modules/gke/output.tf
@@ -7,3 +7,13 @@ output "subnetwork_self_link" {
   value       = google_compute_subnetwork.private.self_link
   description = "The self link of the private subnetwork"
 }
+
+output "cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+  description = "Endpoint for the GKE cluster"
+}
+
+output "cluster_ca_certificate" {
+  value = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  description = "CA certificate for the GKE cluster"
+}
